ff35091d32edb3295fff23f388c3efe6950db625
Shi Wenwu <125130360+Ultraman-Leo-7@users.noreply.github.com>
2026-08-14 19:52:46 +0800

    Remove obsolete v5.2.10 smoke test

--- tests/test_v5210_smoke.py
+++
@@ -1,9 +0,0 @@
-import app
-
-
-def test_app_version_is_v5210():
-    assert app.APP_VERSION == "5.2.10"
-
-
-def test_visible_search_scope_label_is_present():
-    assert "全部显示字段" in app.InvoiceApp._visible_search_scopes.__code__.co_consts
